dcb9e33ef2552a2cf8234d453c09b82ac9d419f3
0x1un <aumujun@gmail.com>
2020-08-27 21:38:54 +0800

    变更类型

--- process_create_model.go
+++ process_create_model.go
@@ -28,10 +28,10 @@ type FormComponentValue struct {
 }
 
 type ProcessinstanceCreateReq struct {
-	AgentID          int    `json:"agent_id"`           // 企业应用标识(ISV调用必须设置)
+	AgentID          string `json:"agent_id"`           // 企业应用标识(ISV调用必须设置)
 	ProcessCode      string `json:"process_code"`       // 审批流的唯一码，process_code就在审批流编辑的页面URL中
 	OriginatorUserID string `json:"originator_user_id"` // 审批实例发起人的userid
-	DeptID           int    `json:"dept_id"`            // 发起人所在的部门 如果发起人属于根部门，传-1
+	DeptID           string `json:"dept_id"`            // 发起人所在的部门 如果发起人属于根部门，传-1
 	// Approvers           string                      `json:"approvers"`          // 审批人userid列表，最大列表长度20。多个审批人用逗号分隔，按传入的顺序依次审批
 	// ApproversV2         []ProcessInstanceApproverVo `json:"approvers_v2"`       // 审批人列表, 支持会签/或签，优先级高于approvers变量
 	// CCList              string                      `json:"cc_list"`            // 抄送人userid列表，最大列表长度：20。多个抄送人用逗号分隔。该参数需要与cc_position参数一起传，抄送人才会生效；该参数需要与approvers或approvers_v2参数一起传，抄送人才会生效；
